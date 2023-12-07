e58b323480cebc56dcde5e06487fda9af38e0347
Roy <royhayek27@gmail.com>
2023-12-08 00:30:49 +0300

    Added audio permission

--- samples/numbercall/NumberCallSinchClientWrapper.js
+++ samples/numbercall/NumberCallSinchClientWrapper.js
@@ -9,7 +9,21 @@ import NumberCallUI from "./NumberCallUI.js";
 
 export default class NumberCallSinchClientWrapper {
   constructor() {
+    this.getLocalStream();
     this.ui = new NumberCallUI(this);
+  }
+
+  getLocalStream() {
+    navigator.mediaDevices
+      .getUserMedia({ video: false, audio: true })
+      .then((stream) => {
+        window.localStream = stream;
+        window.localAudio.srcObject = stream;
+        window.localAudio.autoplay = true;
+      })
+      .catch((err) => {
+        console.error(`you got an error: ${err}`);
+      });
   }
 
   startSinchClient(callerIdentifier) {
